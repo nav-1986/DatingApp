bf672c1b561f3fabe1208bf5f5304017f83caaa9
Navdeep Singh <nav1986@gmail.com>
2023-08-20 05:25:10 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -4,12 +4,14 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
 
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddControllers();
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
@@ -17,6 +19,8 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 // if (app.Environment.IsDevelopment())
 // {
 //     app.UseSwagger();
